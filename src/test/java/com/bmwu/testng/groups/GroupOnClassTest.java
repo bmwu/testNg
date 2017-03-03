7e4a18d3ad36114dbc02a689ee8867d64b73bac0
Michael <Michael@zenzet.com>
2017-03-03 14:12:16 +0800

    group Test

---
+++ src/test/java/com/bmwu/testng/groups/GroupOnClassTest.java
@@ -0,0 +1,14 @@
+package com.bmwu.testng.groups;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by michael on 3/3/17.
+ */
+@Test(groups = "group-test")
+public class GroupOnClassTest {
+
+    public void runGroupOnClass() {
+        System.out.println("runGroupClass");
+    }
+}
